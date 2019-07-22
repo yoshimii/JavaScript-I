781f6c4d5ebc4115041184bce3bfb9fd5f184142
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-07-22 16:24:12 -0500

    completed function conversion assignment

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,26 +1,42 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
-// let myFunction = function () {
-// console.log("Function was invoked!");
-// };
-// myFunction();
+let myFunction = function () {
+console.log("Function was invoked!");
+};
+myFunction();
 
-// let anotherFunction = function (param) {
-//   return param;
-// };
-// anotherFunction("Example");
+myArrowFunction = () => console.log("Arrow function invoked!");
 
-// let add = function (param1, param2) {
-//   return param1 + param2;
-// };
-// add(1,2);
+myArrowFunction();
+/**********************************************************************/
+let anotherFunction = function (param) {
+  return param;
+};
+anotherFunction("Example");
+console.log(anotherFunction("Example"));
 
-// let subtract = function (param1, param2) {
-//   return param1 - param2;
-// };
-// subtract(1,2);
+let anotherArrowFunction = param => param;
+
+console.log(anotherArrowFunction("ArrowExample"));
+/**********************************************************************/
+let add = function (param1, param2) {
+  return param1 + param2;
+};
+add(1,2);
 
 
+let addArrow = (param1, param2) => (param1+param2);
+addArrow(2,4);
+console.log(addArrow(2,4));
+/**********************************************************************/
+let subtract = function (param1, param2) {
+  return param1 - param2;
+};
+subtract(1,2);
+
+let subtractArrow = (param1,param2) => (param1-param2);
+console.log(subtractArrow(2,4);)
+/**********************************************************************/
 // Stretch
 
 // exampleArray = [1,2,3,4];
