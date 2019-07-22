71f4fc1305d4296f2c969fe81b4919171ef6c337
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-07-21 20:11:14 -0500

    arrow function conversion and array practice

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -63,7 +63,22 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
+// let find = inventory.filter(function(car){
+//     ;
+// });
+
+//function declaration to find car id # 33
+let carId = inventory.find(function(car){
+    return car.id ==33;
+})
+
+//Arrow function conversion
+let carIdArrow = inventory.find(car => (car.id == 33));
+//Printing info for car 33 to screen
+console.log(`Car 33 is a ${carId.car_year} ${carId.car_make} ${carId.car_model}.`)
+
+
+// console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
