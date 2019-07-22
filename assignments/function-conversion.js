8f008c2786070306cd353b39c96c6b5c6248d083
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-07-22 16:28:38 -0500

    completed stretch

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -35,12 +35,16 @@ let subtract = function (param1, param2) {
 subtract(1,2);
 
 let subtractArrow = (param1,param2) => (param1-param2);
-console.log(subtractArrow(2,4);)
+console.log(subtractArrow(2,4))
 /**********************************************************************/
 // Stretch
 
-// exampleArray = [1,2,3,4];
-// const triple = exampleArray.map(function (num) {
-//   return num * 3;
-// });
-// console.log(triple);
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map(function (num) {
+  return num * 3;
+});
+console.log(triple);
+
+exampleArrowArray = [2,4,6,8];
+const quadruple = exampleArrowArray.map(num => num * 4);
+console.log(quadruple)
